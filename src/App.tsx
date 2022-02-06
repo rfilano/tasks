6dfed202e8ced8c63ba5e4e48f7e234d052a6046
Rosemarie <90461398+rfilano@users.noreply.github.com>
2022-02-06 18:26:02 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,6 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p>
-                February 06, 2022 18:23:45 - <code>App.tsx</code> sucessfully edited by Rosemarie Filano
-            </p>
         </div>
     );
 }
