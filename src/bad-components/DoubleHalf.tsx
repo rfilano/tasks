d3a409ad3766230262b78ea9ae63049e7982e59d
rfilano <rfilano6@gmail.com>
2022-03-09 16:06:00 -0500

    Fixed ColoredBox.tsx and DoubleHalf.tsx

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+interface dhProps {
+    getdh: () => void;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Doubler({ getdh }: dhProps): JSX.Element {
+    return <Button onClick={getdh}>Double</Button>;
+}
+
+function Halver({ getdh }: dhProps): JSX.Element {
+    return <Button onClick={getdh}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
+    const double = () => setDhValue(2 * dhValue);
+    const half = () => setDhValue(0.5 * dhValue);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler getdh={double}></Doubler>
+            <Halver getdh={half}></Halver>
         </div>
     );
 }
